9f5a876d11fb1fbe98f7974171ee62ff32c9b736
Erkin Eren <ernerkin@gmail.com>
2021-02-24 16:47:31 +0300

    refactoring

--- example/getCategories.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once __DIR__ . '/common.php';
-
-
-$products = AdminModelCatalogCategory::getCategories()->getRawResponse();
-header('Content-Type: text/plain; charset=utf-8;');
-print_r($products);
-
